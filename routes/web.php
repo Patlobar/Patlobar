917db31d29141e42567030a74a6c79a61d4e2ea2
Parlobar <titalobar7@gmail.com>
2024-04-18 10:57:04 -0300

    tela de cadastro

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+Route::view('/', 'home');
+Route::view('/cria-conta', 'cria-conta');
+Route::view('/testedeconteudo', 'teste');
+
